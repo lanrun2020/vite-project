eeeefa99b6b441499e214897e6491a0f820d59eb
lanrun <1947332325@qq.com>
2022-02-10 17:39:44 +0800

    feat:创建vite-project项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <el-button type="primary" @click="count--">count - 1</el-button>
+  <el-button type="primary" @click="count++">count + 1</el-button>
+  <h1>count:{{count}}</h1>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
