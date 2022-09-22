c7bc5494517e59e3a46e96bd8aa25853db8f9714
lanrun <1947332325@qq.com>
2022-09-22 16:07:30 +0800

    设置cesiumtoken

--- src/utils/importCesium.ts
+++ src/utils/importCesium.ts
@@ -1,4 +1,4 @@
 import * as Cesium from 'cesium'
 window.Cesium = Cesium
-// window.Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwZDNlOWQ0ZS05Y2QyLTQzZDgtYmM0My01NjJmMGJmMzBlNTUiLCJpZCI6ODQ5ODQsImlhdCI6MTY2MDcyMTE0N30.Zl3PqwpwxkWgq1WDrnOaGlxVGsDhIFRWP503ukrLD8o"
+window.Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwZDNlOWQ0ZS05Y2QyLTQzZDgtYmM0My01NjJmMGJmMzBlNTUiLCJpZCI6ODQ5ODQsImlhdCI6MTY2MDcyMTE0N30.Zl3PqwpwxkWgq1WDrnOaGlxVGsDhIFRWP503ukrLD8o"
 export default ({ ...window.Cesium })
