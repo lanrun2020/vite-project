e9cd026d8cfef336ecb13fa38860352e2d8ac675
lanrun <1947332325@qq.com>
2023-09-05 23:22:54 +0800

    commit

--- src/views/Cesium/addChangePosition.ts
+++ src/views/Cesium/addChangePosition.ts
@@ -4,7 +4,7 @@ let handler
 export const addChangePosition = async (viewer: any, active: boolean) => {
   if (active) {
     const entity = viewer.entities.add({
-      position: Cesium.Cartesian3.fromDegrees(115.59777, 34.03883,5),
+      position: Cesium.Cartesian3.fromDegrees(115.59777, 34.03883,200),
       billboard: {
         image: river, // default: undefined
         show: true, // default
@@ -18,46 +18,55 @@ export const addChangePosition = async (viewer: any, active: boolean) => {
       name: "Blue dashed line",
       polyline: {
         positions: Cesium.Cartesian3.fromDegreesArrayHeights([
-          115.59777, 34.03883,1,
-          115.59777, 34.03883,5
+          115.59777, 34.03883,0,
+          115.59777, 34.03883,200
         ]),
         width: 4,
         material: new Cesium.PolylineDashMaterialProperty({
           color: Cesium.Color.CYAN,
         }),
       },
-    });
+    });    
     handler = new Cesium.ScreenSpaceEventHandler(viewer.scene.canvas);
     handler.setInputAction((clickEvent: any) => {
-      // console.log(clickEvent);
       const mouseY = clickEvent.position.y
       const mouseX = clickEvent.position.x
       const pick = viewer.scene.pick(clickEvent.position);
+      console.log(clickEvent.position);
+      
       if (pick && pick.id) {
-        // console.log(pick.id.position);
         //1.获取此实体的高度，将线两端的位置转屏幕坐标计算差值
         const cartographic = Cesium.Cartographic.fromCartesian(pick.id.position._value);
-        // console.log(cartographic);
-        const height = cartographic.height
+        const height = cartographic.height //获取实体高度，后续好添加上去
         const latitude = cartographic.latitude
         const longitude = cartographic.longitude
-
         //将高度置0后转连线底部的笛卡尔坐标
         const cartesian3Bottom = Cesium.Cartesian3.fromRadians(longitude,latitude,0)
         //转屏幕坐标
         const cartesianBottom = Cesium.SceneTransforms.wgs84ToWindowCoordinates(viewer.scene, cartesian3Bottom);
-        // console.log(cartesianBottom);
         //2.通过差值换算后通过屏幕坐标推出连线线底部经纬度坐标
         //计算此时点击的位置转屏幕坐标与上面的屏幕坐标的高度差值
         const disY = cartesianBottom.y - mouseY
-
+        const disX = cartesianBottom.x - mouseX
         //3.再添加高度设置实体位置
         handler.setInputAction(function (event) {
           //获取到的鼠标高度减去差值再转经纬度，设置给实体
           const movePosition = event.endPosition
-          movePosition.y -= disY
-          const cartesian = viewer.camera.pickEllipsoid(movePosition, viewer.scene.globe.ellipsoid);
-          pick.id.position = cartesian
+          movePosition.y += disY
+          movePosition.x += disX //这一步换算的坐标会有一定偏差
+          const d = ((movePosition.x - mouseX)/mouseX)*85
+          // console.log(d);
+          
+          movePosition.x = movePosition.x-d
+          const cartesian2 = viewer.camera.pickEllipsoid(movePosition, viewer.scene.globe.ellipsoid);
+          //cartesian此点为连线底部点,接着将这个点高度提升200，再换算出坐标
+          const cartographic2 = Cesium.Cartographic.fromCartesian(cartesian2);
+          const height2 = cartographic2.height
+          const latitude2 = cartographic2.latitude
+          const longitude2 = cartographic2.longitude
+          const cartesian3Top = Cesium.Cartesian3.fromRadians(longitude2,latitude2,height2+height)
+          pick.id.position = cartesian3Top
+          dashedLine.polyline.positions.setValue([cartesian2, cartesian3Top])
         },Cesium.ScreenSpaceEventType.MOUSE_MOVE);
         handler.setInputAction(function () {
           handler.removeInputAction(Cesium.ScreenSpaceEventType.MOUSE_MOVE)//移除事件
