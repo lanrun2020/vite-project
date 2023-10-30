8459dd475b3f7aa149aee63c1997f5bac52c0762
lanrun <1947332325@qq.com>
2023-10-30 17:33:09 +0800

    commit

--- src/views/Cesium/addContourLine.ts
+++ src/views/Cesium/addContourLine.ts
@@ -12,25 +12,35 @@ export const addContourLine = (viewer: any, active: boolean) => {
       const extent = [106.64396446248583,30.077195435475748, 106.80509638145465, 30.209849307650593];
       const cellSide = 0.5;
       const options = {units: 'miles'};
-
       const grid = turf.pointGrid(extent, cellSide, options); //计算点网格
-      console.log(grid);//经纬度
       grid.features.forEach((item) => {
         pos.push(Cesium.Cartographic.fromDegrees(item.geometry.coordinates[0], item.geometry.coordinates[1]))
-        viewer.entities.add({
-          position: Cesium.Cartesian3.fromDegrees(...item.geometry.coordinates),
-          point: {
-            pixelSize: 10,
-            color: Cesium.Color.YELLOW,
-            heightReference: Cesium.HeightReference.CLAMP_TO_GROUND,
-          },
-        })
+        // viewer.entities.add({
+        //   position: Cesium.Cartesian3.fromDegrees(...item.geometry.coordinates),
+        //   point: {
+        //     pixelSize: 10,
+        //     color: Cesium.Color.YELLOW,
+        //     heightReference: Cesium.HeightReference.CLAMP_TO_GROUND,
+        //   },
+        // })
       })
       const promise = Cesium.sampleTerrain(terrainProvider, 11, pos);
+      let arr = []
       Promise.resolve(promise).then(function(updatedPositions) {
-        //console.log(updatedPositions)//弧度加高度
+        const terrainDataPromise = terrainProvider.requestTileGeometry(3263,680,11);
+        console.log(terrainDataPromise);
+        terrainDataPromise.then((res) => {
+          console.log(res);
+          const primitiveShader = viewer.scene.primitives.add(
+            new CustomPrimitive(res._quantizedVertices, res._boundingSphere, res._indices)
+          );
+          const boundingSphere = primitiveShader.boundingSphere;
+          // 将相机定位和视野范围应用于相机
+          viewer.camera.flyToBoundingSphere(boundingSphere)
+          console.log(primitiveShader);
+        })
         //转世界坐标
-        const arr = updatedPositions.map((item) => {
+        arr = updatedPositions.map((item) => {
           //item
           const cartesian3 = new Cesium.Cartesian3();
           Cesium.Ellipsoid.WGS84.cartographicToCartesian(
@@ -40,13 +50,10 @@ export const addContourLine = (viewer: any, active: boolean) => {
           return cartesian3 //笛卡尔世界坐标集合
         })
       })
-      // const primitiveShader = viewer.scene.primitives.add(
-      //   new CustomPrimitive(position)
-      // );
-        viewer.camera.flyTo({
-          destination: Cesium.Cartesian3.fromDegrees(106.70296568464478, 30.209849307650593, 3000.0),
-          duration: 1.6
-        });
+        // viewer.camera.flyTo({
+        //   destination: Cesium.Cartesian3.fromDegrees(106.70296568464478, 30.209849307650593, 3000.0),
+        //   duration: 1.6
+        // });
   } else {
     if (primitives) {
       primitives.removeAll()
@@ -57,43 +64,16 @@ export const addContourLine = (viewer: any, active: boolean) => {
 class CustomPrimitive {
   private show
   private drawCommand: any
-  private faceResolution
-  private cartographicPosition: any
-  private modelMatrix: any
-  private halfWidthMeters: any
   private boundingSphere: any
+  private vertices: any
+  private indices: any
   private time: any
-  constructor(cartographicPosition: any) {
+  constructor(vertices: any, boundingSphere: any, indices:any) {
     this.show = true;
-
-    // This is initialized in the first call of update()
-    // so we don't need a context
+    this.vertices = vertices
+    this.indices = indices
+    this.boundingSphere = boundingSphere
     this.drawCommand = undefined;
-
-    // number of faces wide. There are resolution + 1 vertices.
-    this.faceResolution = 10;
-
-    // Compute a model matrix that puts the surface at a specific point on
-    // the globe.
-    // 计算一个模型矩阵，将表面放在地球上的一个特定点上
-    this.cartographicPosition = cartographicPosition;
-    const cartesianPosition = Cesium.Cartographic.toCartesian(
-      cartographicPosition,
-      Cesium.Ellipsoid.WGS84,
-      new Cesium.Cartesian3()
-    );
-    this.modelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(
-      cartesianPosition,
-      Cesium.Ellipsoid.WGS84,
-      new Cesium.Matrix4()
-    );
-
-    this.halfWidthMeters = 10000;
-    this.boundingSphere = new Cesium.BoundingSphere(
-      cartesianPosition,
-      this.halfWidthMeters * Math.SQRT2
-    );
-
     this.time = undefined;
   }
   //会一直回调,直到销毁
@@ -118,70 +98,6 @@ class CustomPrimitive {
   }
 }
 
-/**
- * generate a quad that's subdivided into faceResolution x faceResolution quads.
- * The vertices will be adjusted in the vertex shader.
- * 生成一个细分为facerresolution x facerresolution的四边形
- * 顶点将在顶点着色器中进行调整
- * 返回每个点位的位置数据
- */
-const generateVertices = (faceResolution: number, halfWidthMeters: number) => {
-  // faceResolution 分辨率 10 ,halfWidthMeters 10000米
-  const vertexResolution = faceResolution + 1;
-  const vertexCount = vertexResolution * vertexResolution;
-  const componentsPerVertex = 3;
-  const vertices = new Float32Array(vertexCount * componentsPerVertex);
-  for (let i = 0; i < vertexResolution; i++) {
-    for (let j = 0; j < vertexResolution; j++) {
-      const u = i / (vertexResolution - 1);
-      const v = j / (vertexResolution - 1);
-      const index = i * vertexResolution + j;
-      const x = halfWidthMeters * (2 * u - 1);
-      const y = halfWidthMeters * (2 * v - 1);
-      const z = 0;
-
-      vertices[index * componentsPerVertex] = x;
-      vertices[index * componentsPerVertex + 1] = y;
-      vertices[index * componentsPerVertex + 2] = z;
-    }
-  }
-  return vertices;
-}
-
-/**
- * Tessellate a big square region into faceResolution x faceResolution quads
- * 将一个大的正方形区域镶嵌为facerresolution x facerresolution四边形，返回点位下标集合
- */
-const generateIndices = (faceResolution: number) => {
-  const indicesPerQuad = 6;
-  const indexCount = faceResolution * faceResolution * indicesPerQuad;
-  const indices = new Uint16Array(indexCount);
-
-  const vertexResolution = faceResolution + 1;
-
-  let quadIndex = 0;
-  for (let i = 0; i < faceResolution; i++) {
-    for (let j = 0; j < faceResolution; j++) {
-      const a = i * vertexResolution + j;
-      const b = i * vertexResolution + (j + 1);
-      const c = (i + 1) * vertexResolution + (j + 1);
-      const d = (i + 1) * vertexResolution + j;
-
-      indices[quadIndex * indicesPerQuad] = a;
-      indices[quadIndex * indicesPerQuad + 1] = b;
-      indices[quadIndex * indicesPerQuad + 2] = c;
-      indices[quadIndex * indicesPerQuad + 3] = c;
-      indices[quadIndex * indicesPerQuad + 4] = d;
-      indices[quadIndex * indicesPerQuad + 5] = a;
-      quadIndex++;
-    }
-  }
-  //[0,1,12,  12,11,0]是第一个格子的两个三角形点位下标集合
-  //一共100个格子，即600个点的下标集合
-
-  return indices;
-}
-
 const initialize = (primitive: any, context: any) => {
   // context绘制的上下文
   // Inteference patterns made by two plane waves crossing each
@@ -195,12 +111,9 @@ const initialize = (primitive: any, context: any) => {
     {
         vec2 k = vec2(0.0004, 0.0001);
         vec2 k2 = vec2(0.0001, 0.0004);
-        float z = 0.0;
-        z += 1000.0 * sin(czm_pi * dot(a_position.xy, k) * 0.5 - 0.002 * u_time);
-        z += 1000.0 * sin(czm_pi * dot(a_position.xy, k2) * 0.5 - 0.002 * u_time);
         v_position = a_position;
-        v_h = z/4000.0 + 0.5;
-        gl_Position = czm_modelViewProjection * vec4(a_position.xy, z, 1.0);
+        v_h = a_position.z/4000.0 + 0.5;
+        gl_Position = czm_modelViewProjection * vec4(a_position.xyz, 1.0);
     }
     `;
 
@@ -210,19 +123,8 @@ const initialize = (primitive: any, context: any) => {
     varying float v_h;
     void main()
     {
-        vec2 k = vec2(0.0004, 0.0001);
-        vec2 k2 = vec2(0.0001, 0.0004);
-        float z = 0.0;
-        z += sin(2.0 * czm_pi * dot(v_position.xy, k) - 0.002 * u_time);
-        z += sin(2.0 * czm_pi * dot(v_position.xy, k2) - 0.002 * u_time);
-        // divide by 2 to keep in the range [-1, 1]
-        z *= 0.5;
-        // signed -> unsigned
-        z = 0.5 + 0.5 * z;
         vec3 color = vec3(0.0,1.0,0.0);
-        color = mix(vec3(0.0,1.0,0.0),vec3(1.0,0.0,0.0),v_h);
-        gl_FragColor = vec4(color, v_h);
-
+        gl_FragColor = vec4(1.0,0.0,0.0, 1.0);
     }
     `;
 
@@ -238,11 +140,7 @@ const initialize = (primitive: any, context: any) => {
     fragmentShaderSource: fragmentShader,
     attributeLocations: {},
   });
-
-  const positionTypedArray = generateVertices(
-    primitive.faceResolution,
-    primitive.halfWidthMeters
-  );//[10000,10000 ~ -10000,-10000之间的点]
+  const positionTypedArray = primitive.vertices;//[10000,10000 ~ -10000,-10000之间的点]
   const positionVertexBuffer = Cesium.Buffer.createVertexBuffer({
     context: context,
     typedArray: positionTypedArray,//[10000,10000 ~ -10000,-10000之间的点]
@@ -257,8 +155,7 @@ const initialize = (primitive: any, context: any) => {
     ),//数据类型
   };
 
-  const indexCount = primitive.faceResolution * primitive.faceResolution * 6; //600
-  const indexTypedArray = generateIndices(primitive.faceResolution);
+  const indexTypedArray = primitive.indices;
   const indexBuffer = Cesium.Buffer.createIndexBuffer({
     context: context,
     typedArray: indexTypedArray,
@@ -292,11 +189,11 @@ const initialize = (primitive: any, context: any) => {
     //虽然未定义是允许的，但总是尝试提供一个边界体积，以允许为场景计算尽可能紧密的近平面和远平面，并最小化所需的截锥体数量。
     //modelMatrix,这里可以传一个空间变换，那么使用的位置数据会根据这个进行变换，如果没有定义，则认为是世界坐标
     //modelMatrix: primitive.modelMatrix, // 从模型空间中的几何图形到世界空间中的变换。如果未定义，则假定几何体在世界空间中定义。
-    pass: Cesium.Pass.TRANSLUCENT, //渲染通道 OPAQUE不透明, OVERLAY覆盖, TRANSLUCENT半透明
+    pass: Cesium.Pass.OPAQUE, //渲染通道 OPAQUE不透明, OVERLAY覆盖, TRANSLUCENT半透明
     shaderProgram: shaderProgram, //着色器程序
     renderState: renderState, //渲染状态
     vertexArray: vertexArray, //顶点数组121 11×11
-    count: indexCount, //顶点数组中要绘制的顶点数，一个正方形网格是两个三角形构成的，一共包含了6个点，100个网格就是600个点位
+    count: 4149, //顶点数组中要绘制的顶点数，一个正方形网格是两个三角形构成的，一共包含了6个点，100个网格就是600个点位
     primitiveType: Cesium.PrimitiveType.TRIANGLES,//顶点数组中的几何类型(TRIANGLES 将一系列点绘制成三角形)
     uniformMap: uniformMap, //一个对象，其函数的名称与着色器程序中的制服相匹配，并返回值来设置这些制服
   });
