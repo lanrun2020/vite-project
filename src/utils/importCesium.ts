f8378d2b80880d3c5210a5f65975ed6097091311
lanrun <1947332325@qq.com>
2023-11-03 17:45:12 +0800

    commit

--- src/utils/importCesium.ts
+++ src/utils/importCesium.ts
@@ -1,4 +1,4 @@
 import * as Cesium from 'cesium'
 window.Cesium = Cesium
-window.Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkZTEzYmM0NS1mZDFkLTQ4NDMtYmYwNC1lZWIwOGE0MTBiMTciLCJpZCI6ODQ5ODQsImlhdCI6MTY5MzM3OTkxMX0.b0mML0vz0kp_yMLwL1y0JDwfcKDwSgFRe8XjPVRaeK0"
+window.Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI5MDJlNGVhMS0wNDE0LTQ1MzQtODA0ZS0zMDFjYjIyNDIyYWMiLCJpZCI6ODQ5ODQsImlhdCI6MTY5ODk3NjMwNX0.l9gePdYL7kgM6ZIpzzInkAAlNiTmLdgpIAUo1NscUxs"
 export default ({ ...window.Cesium })
